00bd12d076806077c1f01ade301431643ac2e656
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2025-07-12 23:06:28 -0300

    feat(api): add DioClient wrapper with auth support

---
+++ lib/core/config/http_client.dart
@@ -0,0 +1,52 @@
+import 'package:dio/dio.dart';
+import '../utils/secure_storage.dart';
+
+class DioClient {
+  final Dio _dio;
+  final SecureStorage _secureStorage;
+
+  DioClient({required Dio dio, required SecureStorage secureStorage})
+    : _dio = dio,
+      _secureStorage = secureStorage;
+
+  Future<Response> get(
+    String endpoint, {
+    bool auth = false,
+    Map<String, dynamic>? queryParameters,
+  }) async {
+    final headers = await _buildHeaders(auth: auth);
+
+    return _dio.get(
+      endpoint,
+      queryParameters: queryParameters,
+      options: Options(headers: headers),
+    );
+  }
+
+  Future<Response> post(
+    String endpoint, {
+    bool auth = false,
+    Map<String, dynamic>? data,
+  }) async {
+    final headers = await _buildHeaders(auth: auth);
+
+    return _dio.post(
+      endpoint,
+      data: data,
+      options: Options(headers: headers),
+    );
+  }
+
+  Future<Map<String, String>> _buildHeaders({bool auth = false}) async {
+    final headers = <String, String>{'Content-Type': 'application/json'};
+
+    if (auth) {
+      final token = await _secureStorage.getAccessToken();
+      if (token != null) {
+        headers['Authorization'] = 'Bearer $token';
+      }
+    }
+
+    return headers;
+  }
+}
