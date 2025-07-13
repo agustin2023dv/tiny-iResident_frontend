996295dd328edf1f60292bc21834a2a52699a487
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2025-07-12 23:06:17 -0300

    feat(network): set up Dio base instance with default config

---
+++ lib/core/config/dio_client.dart
@@ -0,0 +1,10 @@
+import 'package:dio/dio.dart';
+
+final Dio dio = Dio(
+  BaseOptions(
+    baseUrl: 'http://10.0.2.2:8000/api',
+    connectTimeout: const Duration(seconds: 10),
+    receiveTimeout: const Duration(seconds: 10),
+    headers: {'Content-Type': 'application/json'},
+  ),
+);
